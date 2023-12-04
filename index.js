e717211e1147e050b5a518b66ceb00e0bb98545d
fdariasglez@hotmail.com <fdariasglez@hotmail.com>
2023-12-04 13:26:38 +0000

    Colores

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 const path = require('path');
+const color = require('colors');
 const { Client, GatewayIntentBits, PermissionsBitField, ChannelFlagsBitField, ChannelType } = require('discord.js');
+const { send } = require('process');
 require('dotenv').config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', async () => {
-  console.log(`Bot iniciado como ${client.user.tag}`);
+  console.log(`Bot iniciado como ${client.user.tag}`.america);
 
   // Solicitar la ID del servidor desde la línea de comandos
   const readline = require('readline').createInterface({
@@ -14,11 +16,11 @@ client.on('ready', async () => {
     output: process.stdout
   });
 
-  readline.question('Ingrese la ID del servidor que desea borrar: ', (guildId) => {
+  readline.question('Ingrese la ID del servidor que desea borrar: '.cyan, (guildId) => {
     const guild = client.guilds.cache.get(guildId);
 
     if (guild) {
-      console.log(`Operación de borrado iniciada para el servidor ${guild.name} con ID: (${guild.id})`);
+      console.log(`Operación de borrado iniciada para el servidor ${guild.name} con ID: (${guild.id})`.blue);
       deleteAllChannels(guild).then(async () => {
         //Create a channel with the name "new-general" everyone can see but only you can send messages
         await guild.channels.create({
@@ -35,11 +37,12 @@ client.on('ready', async () => {
             },
           ],
         });
+
         //Finaliza el proceso
         process.exit();
       });
     } else {
-      console.log(`No se encontró el servidor con ID: (${guildId})`);
+      console.log(`No se encontró el servidor con ID: (${guildId})`.underline.red);
       return;
     }
   });
@@ -49,10 +52,10 @@ async function deleteAllChannels(guild) {
   try {
     // Eliminar todos los canales
     await Promise.all(guild.channels.cache.map(channel => channel.delete()));
-    console.log('Todos los canales eliminados');
+    console.log('Todos los canales eliminados'.brightMagenta);
 
   } catch (error) {
-    console.error('Error al eliminar canales:', error);
+    console.error('Error al eliminar canales:'.underline.red, error);
     throw error;
   }
 }
